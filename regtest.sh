2656c6c173d1a31aeba0e79c0a2fcb7b5e0e7a7a
Ben England <bengland@redhat.com>
2013-07-26 16:12:41 -0400

    fix regression test bugs with NFS

--- regtest.sh
+++ regtest.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # smallfile regression test
+# NOTE: this expects you to have /var/tmp in a filesystem that supports extended attributes
+# and it expects NFS to support extended attributes
 
 localhost_name="$1"
-if [ -z "$localhost_name" ] ; then localhost_name="`hostname -s`" ; fi
+if [ -z "$localhost_name" ] ; then localhost_name="localhost" ; fi
 
-testdir='/var/tmp/smfregtest'
+testdir='$TMPDIR/smfregtest'
+if [ "$TMPDIR" = "" ] ; then
+  testdir='/var/tmp/smfregtest'
+fi
 nfsdir=/var/tmp/smfnfs
 OK=0
 NOTOK=1
+GREP="grep -q "
 
 assertfail() {
   status=$1
@@ -26,9 +32,30 @@ assertok() {
 }
 
 cleanup() {
-  rm -rf /var/tmp/invoke*.log $testdir
-  mkdir $testdir
+  rm -rf /var/tmp/invoke*.log $testdir/*
+  mkdir -p $testdir
 }
+
+
+# set up NFS mountpoint
+
+$GREP $nfsdir /proc/mounts
+if [ $? != $OK ] ; then
+  mkdir -pv $nfsdir
+  rm -rf $testdir
+  mkdir -p $testdir
+  sudo service nfs restart
+  if [ $? != $OK ] ; then 
+    echo "NFS service startup failed!"
+    exit $NOTOK
+  fi
+  sudo exportfs -v -o rw,no_root_squash,sync localhost:$testdir
+  sudo mount -v -t nfs -o actimeo=1 $localhost_name:$testdir $nfsdir
+  if [ $? != $OK ] ; then 
+    echo "NFS mount failed!"
+    exit $NOTOK
+  fi
+fi
 
 # run the smallfile.py module's unit test
 
@@ -37,69 +64,62 @@ python smallfile.py
 assertok $?
 
 # run the invoke_process.py unit test
+
 echo "running invoke_process.py unit test"
 python invoke_process.py
 assertok $?
 
 # run drop_buffer_cache.py unit test
+
 echo "running drop_buffer_cache.py unit test"
 python drop_buffer_cache.py
 assertok $?
 
-# set up NFS mountpoint
-
-grep $nfsdir /proc/mounts
-if [ $? != $OK ] ; then
-  mkdir -pv $nfsdir
-  rm -rf $testdir
-  mkdir -p $testdir
-  sudo mount -v -t nfs -o actimeo=1 $localhost_name:$nfsdir $test
-fi
-
 # test parsing
 
 echo "testing parsing"
-s="./smallfile_cli.py --top $testdir "
+scmd="./smallfile_cli.py --top $testdir "
 f=smfregtest.log
 cleanup
-$s --files 0 > $f
+$scmd --files 0 > $f
 assertfail $?
-grep 'non-negative' $f
+$GREP 'non-negative' $f
 assertok $?
 
 cleanup
-$s --threads 0 > $f
+$scmd --threads 0 > $f
 assertfail $?
-grep 'non-negative' $f
+$GREP 'non-negative' $f
 assertok $?
 
-$s --files -1 > $f
+$scmd --files -1 > $f
 assertfail $?
-$s --record-size -1 > $f
+$scmd --record-size -1 > $f
 assertfail $?
-$s --file-size -1 > $f
+$scmd --file-size -1 > $f
 assertfail $?
-$s --files-per-dir 0 > $f
+$scmd --files-per-dir 0 > $f
 assertfail $?
-$s --dirs-per-dir 0 > $f
+$scmd --dirs-per-dir 0 > $f
 assertfail $?
-$s --record-size -1  > $f
+$scmd --record-size -1  > $f
 assertfail $?
-$s --record-size a > $f
+$scmd --record-size a > $f
 assertfail $?
-$s --top / > $f
+$scmd --top / > $f
 assertfail $?
-$s --response-times foo > $f
+$scmd --response-times foo > $f
 assertfail $?
-$s --stonewall foo > $f
+$scmd --stonewall foo > $f
 assertfail $?
-$s --finish foo > $f
+$scmd --finish foo > $f
 assertfail $?
 
 # run a command with all CLI options and verify that they were successfully parsed
 
 cleanup
-$s --verify-read N --response-times Y --finish N --stonewall N --permute-host-dirs Y --top $nfsdir/smf --same-dir Y --operation cleanup --threads 5 --files 20 --files-per-dir 5 --dirs-per-dir 3 --record-size 6 --file-size 30 --file-size-distribution exponential --prefix a --suffix b --hash-into-dirs Y --pause 5 --host-set $localhost_name | tee $f
+mkdir -p $nfsdir/smf
+$scmd --verify-read N --response-times Y --finish N --stonewall N --permute-host-dirs Y --top $nfsdir/smf --same-dir Y --operation cleanup --threads 5 --files 20 --files-per-dir 5 --dirs-per-dir 3 --record-size 6 --file-size 30 --file-size-distribution exponential --prefix a --suffix b --hash-into-dirs Y --pause 5 --host-set $localhost_name | tee $f
 assertok $?
 expect_strs=( 'verify read? : N' \
         "hosts in test : \['$localhost_name'\]" \
@@ -131,31 +151,40 @@ expect_ct=${#expect_strs[*]}
 for j in `seq 1 $expect_ct` ; do 
   ((k = $j - 1))
   expected_str="${expect_strs[$k]}"
-  echo "expecting: $expected_str"
-  grep "$expected_str" $f
-  assertok $?
+  $GREP "$expected_str" $f
+  s=$?
+  if [ $s != $OK ] ; then 
+    echo "expecting: $expected_str"
+  fi
+  assertok $s
 done
 
-pass1="$s --files 100 --files-per-dir 5 --dirs-per-dir 2 --threads 4 --file-size 4 --record-size 16 --file-size 32  --response-times N --pause 1000 --xattr-count 9 --xattr-size 253"
+common_params="$scmd --files 100 --files-per-dir 5 --dirs-per-dir 2 --threads 4 --file-size 4 --record-size 16 --file-size 32  --response-times N --pause 1000 --xattr-count 9 --xattr-size 253"
 
-allops="cleanup create append read readdir ls-l chmod stat setxattr getxattr symlink mkdir rmdir rename delete-renamed swift-put swift-get cleanup"
+allops=\
+"cleanup create append read readdir ls-l chmod stat setxattr getxattr symlink mkdir rmdir rename delete-renamed swift-put swift-get "
+
+# with NFS, any operation using extended attributes is not supported
+nfs_allops=\
+"cleanup create append read readdir ls-l chmod stat symlink mkdir rmdir rename delete-renamed "
+
+# for debug: allops="create cleanup"
 
 echo "******** testing non-distributed operations"
 for op in $allops ; do
   rm -rf /var/tmp/invoke*.log
-  $pass1 --top $testdir --operation $op
+  $common_params --top $testdir --operation $op
   assertok $?
 done
 
 echo "******** testing distributed operations"
-pass1="$pass1 --host-set $localhost_name"
-for op in $allops ; do
+for op in $nfs_allops ; do
   rm -rf /var/tmp/invoke*.log
-  $pass1 --top $nfsdir/smf --operation $op
+  $common_params --top $nfsdir/smf --host-set $localhost_name --operation $op
   assertok $?
 done
 
-grep $nfsdir /proc/mounts
+$GREP $nfsdir /proc/mounts
 if [ $? == $OK ] ; then
   sudo umount -v $nfsdir
 fi
