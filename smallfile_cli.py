eb0c93ae75c740bd3094312a8d16ea23349f041b
Ben England <bengland@redhat.com>
2013-08-12 10:47:51 -0400

    refactor code to eliminate duplication, make it easier to understand

--- smallfile_cli.py
+++ smallfile_cli.py
@@ -34,69 +34,46 @@ import shutil
 import ssh_thread
 import smallfile
 from smallfile import smf_invocation, ensure_deleted, ensure_dir_exists, get_hostname, hostaddr
-from smallfile import OK, NOTOK, SMFResultException
+from smallfile import OK, NOTOK
 import invoke_process
 import sync_files
-from sync_files import create_top_dirs
 import output_results
+import smf_test_params
+import multi_thread_workload
 
 # FIXME: should be monitoring progress, not total elapsed time
 min_files_per_sec = 15 
 pct_files_min = 70  # minimum percentage of files for valid test
 
-# abort routine just cleans up threads
+# run a multi-host test
 
-def abort_test(abort_fn, thread_list):
-    try:
-      open(abort_fn, "w")
-    except Exception as e:
-      pass
-    for t in thread_list:
-        t.terminate()
+def run_multi_host_workload(prm):
 
-# main routine that does everything for this workload
+    prm_host_set = prm.host_set
+    prm_slave = prm.is_slave
+    prm_permute_host_dirs = prm.permute_host_dirs
+    master_invoke = prm.master_invoke
 
-def run_workload():
-  # if a --host-set parameter was passed, it's a multi-host workload
-  # and we have to fire up this program in parallel on the remote hosts
-  # each remote instance will wait until all instances have reached starting gate
-   
-  (prm_host_set, prm_thread_count, master_invoke, remote_cmd, prm_slave, prm_permute_host_dirs) = parse.parse()
-  starting_gate = master_invoke.starting_gate
-  verbose = master_invoke.verbose
-  host = master_invoke.onhost
+    starting_gate = master_invoke.starting_gate
+    verbose = master_invoke.verbose
+    host = master_invoke.onhost
 
-  # calculate timeouts to allow for initialization delays while directory tree is created
+    # construct list of ssh threads to invoke in parallel
 
-  startup_timeout = 20
-  dirs = master_invoke.iterations * prm_thread_count / master_invoke.files_per_dir
-  if dirs > 20:
-    dir_creation_timeout = dirs / 5 
-    if verbose: print 'extending initialization timeout by %d seconds for directory creation'%dir_creation_timeout
-    startup_timeout += dir_creation_timeout
-  host_startup_timeout = startup_timeout + 10
-
-  # for multi-host test
-
-  if prm_host_set and not prm_slave:
-    host_startup_timeout += len(prm_host_set)/30
-    create_top_dirs(master_invoke, True)
-
-    # construct list of remote host threads
-
-    remote_cmd += ' --slave Y '
+    sync_files.create_top_dirs(master_invoke, True)
+    sync_files.write_pickle(os.path.join(prm.master_invoke.network_dir,'param.pickle'), prm)
+      
     ssh_thread_list = []
     host_ct = len(prm_host_set)
     for j in range(0, len(prm_host_set)):
         n = prm_host_set[j]
-        this_remote_cmd = remote_cmd
+        this_remote_cmd = '%s/smallfile_remote.py --network-sync-dir %s '%(prm.remote_pgm_dir, prm.master_invoke.network_dir)
+        #this_remote_cmd = remote_cmd
         if prm_permute_host_dirs:
           this_remote_cmd += ' --as-host %s'%prm_host_set[(j+1)%host_ct]
 	else:
 	  this_remote_cmd += ' --as-host %s'%n
         if verbose: print this_remote_cmd
-        pickle_fn = master_invoke.host_result_filename(n)
-        ensure_deleted(pickle_fn)
         ssh_thread_list.append(ssh_thread.ssh_thread(n, this_remote_cmd))
 
     # start them, pacing starts so that we don't get ssh errors
@@ -116,12 +93,14 @@ def run_workload():
     sec_delta = 0.5
     try:
      # FIXME: make timeout criteria be that new new hosts seen in X seconds
-     while sec < host_startup_timeout:
+     while sec < prm.host_startup_timeout:
       os.listdir(master_invoke.network_dir)
       hosts_ready = True
+      if os.path.exists(abortfn): raise Exception('worker host signaled abort')
       for j in range(last_host_seen+1, len(prm_host_set)-1):
         h=prm_host_set[j]
         fn = master_invoke.gen_host_ready_fname(h.strip())
+        if verbose: print 'checking for host filename '+fn
         if not os.path.exists(fn):
             hosts_ready = False
             break
@@ -134,14 +113,13 @@ def run_workload():
       time.sleep(sec_delta)
       sec += sec_delta
       sec_delta += 1
-      print 'last_host_seen=%d sec=%d'%(last_host_seen,sec)
+      if verbose: print 'last_host_seen=%d sec=%d'%(last_host_seen,sec)
     except KeyboardInterrupt, e:
      print 'saw SIGINT signal, aborting test'
      hosts_ready = False
     if not hosts_ready:
-      with open(abortfn, "w") as f:
-        f.close()
-      raise Exception('hosts did not reach starting gate within %d seconds'%host_startup_timeout)
+      smallfile.abort_test(abortfn, [])
+      raise Exception('hosts did not reach starting gate within %d seconds'%prm.host_startup_timeout)
 
     # ask all hosts to start the test
     # this is like firing the gun at the track meet
@@ -185,7 +163,7 @@ def run_workload():
                 if e.errno != errno.ENOENT: raise e
                 print '  pickle file %s not found'%pickle_fn
 
-      output_results.output_results(invoke_list, prm_host_set, prm_thread_count,pct_files_min)
+      output_results.output_results(invoke_list, prm_host_set, prm.thread_count,pct_files_min)
 
     except IOError, e:
         print 'host %s filename %s: %s'%(h, pickle_fn, str(e))
@@ -197,135 +175,20 @@ def run_workload():
         sys.exit(NOTOK)
     sys.exit(OK)
 
-  # what follows is code that gets done on each host
-  # if --host-set option is not used, then 
-  # this is all that gets run
 
-  if not prm_slave:
-      create_top_dirs(master_invoke, False)
-  else:
-      time.sleep(1.1)
-      os.listdir(master_invoke.network_dir)
-  for dlist in [ master_invoke.src_dirs, master_invoke.dest_dirs ]:
-    for d in dlist:
-        os.listdir(d)
-        if verbose: print host + ' saw ' + d
+# main routine that does everything for this workload
 
-  # for each thread set up smf_invocation instance,
-  # create a thread instance, and delete the thread-ready file 
+def run_workload():
+  # if a --host-set parameter was passed, it's a multi-host workload
+  # each remote instance will wait until all instances have reached starting gate
+   
+  params = parse.parse()
 
-  thread_list=[]
-  for k in range(0,prm_thread_count):
-    nextinv = smallfile.smf_invocation.clone(master_invoke)
-    nextinv.tid = "%02d"%k
-    if not master_invoke.is_shared_dir:
-        nextinv.src_dirs = [ d + os.sep + master_invoke.onhost + os.sep + "d" + nextinv.tid \
-                             for d in nextinv.src_dirs ]
-        nextinv.dest_dirs = [ d + os.sep + master_invoke.onhost + os.sep + "d" + nextinv.tid \
-                             for d in nextinv.dest_dirs ]
-    t = invoke_process.subprocess(nextinv)
-    thread_list.append(t)
-    ensure_deleted(nextinv.gen_thread_ready_fname(nextinv.tid))
+  # for multi-host test
 
-  starting_gate = thread_list[0].invoke.starting_gate
-  my_host_invoke = thread_list[0].invoke
-
-  # start threads, wait for them to reach starting gate
-  # to do this, look for thread-ready files 
-
-  for t in thread_list:
-    ensure_deleted(t.invoke.gen_thread_ready_fname(t.invoke.tid))
-  for t in thread_list:
-    t.start()
-  if verbose: print "started %d worker threads on host %s"%(len(thread_list),host)
-
-  # wait for all threads to reach the starting gate
-  # this makes it more likely that they will start simultaneously
-
-  threads_ready = False  # really just to set scope of variable
-  k=0
-  for sec in range(0, startup_timeout*2):
-    threads_ready = True
-    for t in thread_list:
-        fn = t.invoke.gen_thread_ready_fname(t.invoke.tid)
-        if not os.path.exists(fn): 
-            threads_ready = False
-            break
-    if threads_ready: break
-    time.sleep(0.5)
-
-  # if all threads didn't make it to the starting gate
-
-  if not threads_ready: 
-    abort_test(my_host_invoke.abort_fn(), thread_list)
-    raise Exception('threads did not reach starting gate within %d sec'%startup_timeout)
-
-  # declare that this host is at the starting gate
-
-  with open(my_host_invoke.gen_host_ready_fname(), "w+") as f:
-    f.close()
-
-  sg = my_host_invoke.starting_gate
-  if not prm_slave: # special case of no --host-set parameter
-    try:
-      sync_files.write_sync_file(sg, 'hi there')
-      if verbose: print 'wrote starting gate file'
-    except IOError, e:
-      print 'error writing starting gate for threads: %s'%str(e)
-
-  # wait for starting_gate file to be created by test driver
-  # every second we resume scan from last host file not found
-  
-  if prm_slave:
-    if prm_host_set == None: prm_host_set = [ host ]
-    for sec in range(0, host_startup_timeout*2):
-      if os.path.exists(sg):
-        break
-      time.sleep(0.5)
-    if not os.path.exists(sg):
-      abort_test(my_host_invoke.abort_fn(), thread_list)
-      raise Exception('starting signal not seen within %d seconds'%host_startup_timeout)
-  if verbose: print "starting test on host " + host + " in 2 seconds"
-  time.sleep(2 + random.random())  
-
-  # FIXME: don't timeout the test, 
-  # instead check thread progress and abort if you see any of them stalled
-  # for long enough
-  # problem is: if servers are heavily loaded you can't use filesystem to communicate this
-
-  # wait for all threads on this host to finish
-
-  for t in thread_list: 
-    if verbose: print 'waiting for thread %s'%t.invoke.tid
-    t.invoke = t.receiver.recv()  # must do this to get results from sub-process
-    t.join()
-
-  # if not a slave of some other host, print results (for this host)
-
-  exit_status = OK
-  if not prm_slave:
-    try: 
-      # FIXME: code to aggregate results from list of invoke objects can be shared by multi-host and single-host cases
-      invoke_list = map( lambda(t) : t.invoke, thread_list )
-      output_results.output_results( invoke_list, [ 'localhost' ], prm_thread_count, pct_files_min )
-    except SMFResultException as e:
-        print 'ERROR: ' + str(e)
-        exit_status = NOTOK
-
-
-  else:
-    # if we are participating in a multi-host test 
-    # then write out this host's result in pickle format so test driver can pick up result
-
-    result_filename = master_invoke.host_result_filename()
-    invok_list = []
-    for t in thread_list:
-        invok_list.append(t.invoke)
-    if verbose: print 'saving result to filename %s'%result_filename
-    sync_files.write_pickle(result_filename, invok_list)
-    time.sleep(1.2) # for benefit of NFS with actimeo=1
-
-  sys.exit(exit_status)
+  if params.host_set and not params.is_slave:
+    return run_multi_host_workload(params)
+  return multi_thread_workload.run_multi_thread_workload(params)
 
 # for future windows compatibility, all global code (not contained in a class or subroutine)
 # must be moved to within a routine unless it's trivial (like constants)
